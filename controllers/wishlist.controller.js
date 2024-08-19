a80ac1faf21834699cbde8127478f8d64cdf9f75
harshitadatra <57798468+harshitadatra@users.noreply.github.com>
2024-08-19 17:29:51 +0530

    feat: added wishlist controller

---
+++ controllers/wishlist.controller.js
@@ -0,0 +1,75 @@
+const User = require("../models/user.model");
+
+const getWishlistItemsHandler = async (req, res) => {
+  try {
+    const userId = req.userId;
+    const user = await User.findById(userId);
+    const { wishlist } = user;
+    return res.status(200).json({ wishlist });
+  } catch (e) {
+    return res.status(500).json({
+      message: "Couldn't get wishlost items.Pleas try again later",
+    });
+  }
+};
+const postItemToWishlistHandler = async (req, res) => {
+  try {
+    const userId = req.userId;
+    const user = await User.findById(userId);
+    const { product } = req.body;
+    const updatedWishlist = [product, ...user.wishlist];
+    const updatedUser = await User.findByIdAndUpdate(
+      userId,
+      {
+        $set: {
+          wishlist: updatedWishlist,
+        },
+      },
+      {
+        new: true,
+      }
+    );
+
+    return res.status(200).json({
+      wishlist: updatedUser.wishlist,
+    });
+  } catch (e) {
+    res.status(500).json({
+      message: "Couldn't post item to wishlist.Please try again later",
+    });
+  }
+};
+
+const deleteItemFromWishlistHandler = async (req, res) => {
+  try {
+    const userId = req.userId;
+    const { user } = await User.find(userId);
+    const { productId } = req.params;
+    let wishlist = user.wishlist;
+    if (!wishlist.find((wishlistItem) => wishlistItem.id === productId)) {
+      return res.status(400).json({
+        message: "Could not find product in wishlist",
+      });
+    }
+
+    wishlist = wishlist.filter((wishlistItem) => wishlist.id !== wishlist);
+    const updatedUser = await User.findByIdUpdate(
+      userId,
+      {
+        $set: {
+          wishlist: wishlist,
+        },
+      },
+      { new: true }
+    );
+  } catch (e) {
+    return res.status(500).json({
+      message: "Couldn't delete item from wishlist.Plese try again later",
+    });
+  }
+};
+module.exports = {
+  getWishlistItemsHandler,
+  postItemToWishlistHandler,
+  deleteItemFromWishlistHandler,
+};
