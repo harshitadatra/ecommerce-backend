86f7bb25de44003114735033073a30e6557c62a2
harshitadatra <57798468+harshitadatra@users.noreply.github.com>
2024-08-19 20:23:53 +0530

    feat: order controller added

---
+++ controllers/orders.controller.js
@@ -0,0 +1,41 @@
+const User = require("../models/user.model");
+
+const getAllOrdersHandler = async (req, res) => {
+  try {
+    const userId = req.userId;
+    const user = await User.findById(userId);
+    const { orders } = user;
+    res.status(200).json({ orders });
+  } catch (e) {
+    return res.status(500).json({
+      message: "Couldn't get order.Please try again later",
+    });
+  }
+};
+
+const postItemToOrdersHandler = async (req, res) => {
+  try {
+    const userId = req.userId;
+    const user = await User.findById({ userId });
+    const { order } = req.body;
+    const updatedOrders = [order, ...user.orders];
+    const updatedUser = await User.findByIdAndUpdate(
+      userId,
+      {
+        $set: {
+          orders: updatedOrders,
+        },
+      },
+      { new: true }
+    );
+    return res.status(201).json({ orders: updatedUser.orders });
+  } catch (e) {
+    return res.status(500).json({
+      message: "Couldn't post item to orders. Please try again later.",
+    });
+  }
+};
+module.export = {
+  getAllOrdersHandler,
+  postItemToOrdersHandler,
+};
