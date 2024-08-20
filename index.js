ba915d81517af436d2b7fa4353562906250d169e
harshitadatra <57798468+harshitadatra@users.noreply.github.com>
2024-08-20 12:31:12 +0530

    feat: added user router

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const router = express.Router();
 const authRoutes = require("./routes/auth.router");
 const productRoutes = require("./routes/product.router");
 const categoryRoutes = require("./routes/category.router");
+const userRoutes = require("./user.router");
+const verifyAuth = require("./middleware/verifyAuth");
+
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const connectDB = require("./db");
@@ -16,6 +19,8 @@ app.use(express.json());
 app.use("/auth", authRoutes);
 app.use("/product", productRoutes);
 app.use("/category", categoryRoutes);
+router.use("/user", verifyAuth, userRoutes);
+
 app.listen(3000, () => {
   console.log("server is running");
 });
