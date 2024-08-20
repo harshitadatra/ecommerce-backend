4e0190a2ef01341c837d60c8378ab678ec2592dc
harshitadatra <57798468+harshitadatra@users.noreply.github.com>
2024-08-20 12:48:44 +0530

    update: user schema

--- models/user.model.js
+++ models/user.model.js
@@ -1,14 +1,88 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const userSchema = Schema({
-  firstName: String,
-  lastName: String,
-  email: { type: String, unique: true },
-  password: String,
-});
+const cartSchema = Schema(
+  {
+    id: String,
+    title: String,
+    imageUrl: String,
+    rating: String,
+    totalRatings: Number,
+    price: String,
+    categoryName: String,
+    isOutOfStock: Boolean,
+    offerPercentage: Number,
+    item: String,
+    isBestSeller: Boolean,
+    priceCategory: String,
+    qty: Number,
+  },
+  {
+    timestamps: true,
+  }
+);
 
-const User = mongoose.model("user", userSchema);
-module.exports = {
-  User,
-};
+const wishlistSchema = Schema(
+  {
+    id: String,
+    title: String,
+    imageUrl: String,
+    rating: String,
+    totalRatings: Number,
+    price: String,
+    categoryName: String,
+    isOutOfStock: Boolean,
+    offerPercentage: Number,
+    item: String,
+    isBestSeller: Boolean,
+    priceCategory: String,
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const addressSchema = Schema(
+  {
+    name: String,
+    street: String,
+    city: String,
+    state: String,
+    zipCode: String,
+    mobile: String,
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const orderSchema = Schema(
+  {
+    items: [cartSchema],
+    orderDate: Date,
+    paymentId: String,
+    totalPrice: Number,
+    deliveryAddress: addressSchema,
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const userSchema = Schema(
+  {
+    firstName: String,
+    lastName: String,
+    email: String,
+    password: String,
+    cart: [cartSchema],
+    wishlist: [wishlistSchema],
+    orders: [orderSchema],
+    address: [addressSchema],
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("User", userSchema);
